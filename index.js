c0a6de1fcd385a60517df81adb1d2d09a33e70d7
mdolic <mirzetdolic2@gmail.com>
2018-11-19 06:37:13 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res)=> {
+    res.send({hi: 'there'});
+});
+
+//localhost:5000
+const PORT = process.env.PORT || 5000; //heroku assigned to port otherwise local 5000
+
+app.listen(PORT, ()=>{
+    console.log(`app starting right now...`);
+});
