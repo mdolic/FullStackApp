be2399ff6d753a3a6be06ad6a8294c65ff2c0bc9
mdolic <mirzetdolic2@gmail.com>
2018-11-21 06:55:42 -0600

    installed passport.js for google oauth

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req,res)=> {
-    res.send({greeting: 'hi there'});
-});
+passport.use(new GoogleStrategy()); // new instance of passport js strategy
+
 
 //localhost:5000
 const PORT = process.env.PORT || 5000; //heroku assigned to port otherwise local 5000
