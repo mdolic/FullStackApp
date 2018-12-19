3b835d4c6dffaed897ed7f8da3ccbbe71e93447b
mdolic <mirzetdolic2@gmail.com>
2018-12-19 10:08:35 -0600

    updated

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,10 +4,19 @@ import React, {Component} from 'react';
 class Header extends Component{
     render(){
         return(
-            <div>
-                Header
-            </div>
-        )
+            <nav>
+                <div className="nav-wrapper">
+                    <a className="left brand-logo">
+                         SurveyJockey
+                    </a>
+                    <ul className="right">
+                        <li>
+                            <a> Login With Google</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        );
     }
 }
 export default Header;
