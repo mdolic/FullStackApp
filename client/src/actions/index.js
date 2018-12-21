59582b0672f26a1941ac6e0a685a6cbc9ad33cbe
mdolic <mirzetdolic2@gmail.com>
2018-12-21 06:22:32 -0600

    finished v1

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,17 @@
 import axios from 'axios';
 import {FETCH_USER} from './types';
 
+/*
+//action creator called, returns function, redux thunk see function return
+calls with dispatch and we then make a request. we wait until we get a response back from our api
+than finally actually dispatch action
+*/
 const fetchUser = ()=>{
-    axios.get('/api/current_user');
+   return function(dispatch){
+     axios
+        .get('/api/current_user')
+        .then(res =>dispatch({type: FETCH_USER, payload:res}));
+
+   }
 
 };
