2ebcfe90427473c9838a4278e6e91bea74709376
mdolic <mirzetdolic2@gmail.com>
2018-12-22 10:34:38 -0600

    updated

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -6,10 +6,8 @@ import {FETCH_USER} from './types';
 calls with dispatch and we then make a request. we wait until we get a response back from our api
 than finally actually dispatch action
 */
-export const fetchUser = ()=>{
-   return function(dispatch){
-     axios
-        .get('/api/current_user')
-        .then(res =>dispatch({type: FETCH_USER, payload:res}));
-   }
-};
+export const fetchUser = ()=> async dispatch =>{
+   const res = await axios.get('/api/current_user')
+        dispatch({type: FETCH_USER, payload:res});
+   };
+4
